4dd12d053098a79029fd3b86bb183121461f1b9a
eldiiar Almazbek <eldiiaralmazbekov@gmail.com>
2022-08-26 11:09:44 +0600

    permission

--- lib/notification_service.dart
+++ lib/notification_service.dart
@@ -1,52 +1,5 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-// final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-
-// Future<void> initNotification() async {
-//   // Android initialization
-//   const initializationSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
-
-//   // Ios initialization
-//   const initializationSettingsIOS = IOSInitializationSettings(
-//     requestAlertPermission: false,
-//     requestBadgePermission: false,
-//     requestSoundPermission: false,
-//   );
-
-//   const initializationSettings = InitializationSettings(
-//     android: initializationSettingsAndroid,
-//     iOS: initializationSettingsIOS,
-//   );
-
-//   // the initialization settings are initialized after they are setted
-//   await flutterLocalNotificationsPlugin.initialize(initializationSettings);
-// }
-
-// Future<void> showNotification(int id, String title, String body) async {
-//   await flutterLocalNotificationsPlugin.show(
-//     id,
-//     title,
-//     body,
-//     const NotificationDetails(
-//       // Android details
-//       android: AndroidNotificationDetails(
-//         'main_channel',
-//         'Main Channel',
-//         channelDescription: "ashwin",
-//         importance: Importance.max,
-//         priority: Priority.max,
-//       ),
-//       // iOS details
-//       iOS: IOSNotificationDetails(
-//         sound: 'default.wav',
-//         presentAlert: true,
-//         presentBadge: true,
-//         presentSound: true,
-//       ),
-//     ),
-//   );
-// }
-
 class NotificationService {
   static final NotificationService _notificationService = NotificationService._internal();
 
@@ -64,9 +17,9 @@ class NotificationService {
 
     // ios initialization
     const initializationSettingsIOS = IOSInitializationSettings(
-      requestAlertPermission: false,
-      requestBadgePermission: false,
-      requestSoundPermission: false,
+      requestAlertPermission: true,
+      requestBadgePermission: true,
+      requestSoundPermission: true,
     );
 
     const initializationSettings = InitializationSettings(
