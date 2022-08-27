addd8dfee8577f02bf19cbfa008d96200c5a97d2
Eldar2021 <eldiiaralmazbekov@gmail.com>
2022-08-27 17:38:32 +0600

    added if for android

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:notification_work_manager/fetch_data.dart';
@@ -11,40 +12,31 @@ Future<void> main() async {
   log('message');
   WidgetsFlutterBinding.ensureInitialized();
   // await NotificationService().initNotification();
-  await Workmanager().initialize(callbackDispatcher, isInDebugMode: false);
-  Workmanager().registerOneOffTask(
-    "task-identifier",
-    'task-identifier',
-    initialDelay: const Duration(seconds: 10),
-    // constraints: Constraints(
-    //   // connected or metered mark the task as requiring internet
-    //   // networkType: NetworkType.connected,
-    //   // require external power
-    //   requiresCharging: true,
-    // ),
-  );
-  // await Workmanager().registerOneOffTask(
-  //   "task-identifier",
-  //   "simpleTask",
-  //   initialDelay: const Duration(seconds: 15),
-  //   // frequency: const Duration(minutes: 15),
-  // );
+  if (Platform.isAndroid) {
+    await Workmanager().initialize(callbackDispatcher, isInDebugMode: true);
+    await Workmanager().registerPeriodicTask(
+      "task-identifier",
+      "simpleTask",
+      initialDelay: const Duration(seconds: 15),
+      frequency: const Duration(minutes: 15),
+    );
+  }
+
   runApp(const MyApp());
 }
 
 Future<void> callbackDispatcher() async {
   Workmanager().executeTask((task, inputData) async {
     final res = await fetchData();
-
     res.fold(
       (l) async {
         log(l.toString());
-        // await NotificationService().showNotification(0, 'Exception1', l.toString());
+        await NotificationService().showNotification(0, 'Exception1', l.toString());
       },
       (r) async {
         log(r.toString());
-        // await NotificationService().showNotification(1, r.msg1.title, r.msg1.content);
-        // await NotificationService().showNotification(2, r.msg2.title, r.msg2.content);
+        await NotificationService().showNotification(1, r.msg1.title, r.msg1.content);
+        await NotificationService().showNotification(2, r.msg2.title, r.msg2.content);
       },
     );
 
