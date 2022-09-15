1310627f7332f90e1de897462ee94a6c45d68546
Eldar2021 <eldiiaralmazbekov@gmail.com>
2022-09-15 10:21:51 +0600

    rum m1 01

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,9 +1,4 @@
-import 'dart:developer';
-import 'dart:io';
-
-import 'package:auto_start_flutter/auto_start_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -14,24 +9,24 @@ class HomeView extends StatefulWidget {
 
 class _HomeViewState extends State<HomeView> {
   @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) async {
-      log('bastaldy');
-      if (Platform.isAndroid) {
-        try {
-          //check auto-start availability.
-          var test = await (isAutoStartAvailable);
-          log(test.toString());
-          //if available then navigate to auto-start setting page.
-          if (test != null && !test) await getAutoStartPermission();
-        } on PlatformException catch (e) {
-          log(e.toString());
-        }
-        if (!mounted) return;
-      }
-    });
-  }
+  // void initState() {
+  //   super.initState();
+  //   WidgetsBinding.instance.addPostFrameCallback((_) async {
+  //     // log('bastaldy');
+  //     // if (Platform.isAndroid) {
+  //     //   try {
+  //     //     //check auto-start availability.
+  //     //     var test = await (isAutoStartAvailable);
+  //     //     log(test.toString());
+  //     //     //if available then navigate to auto-start setting page.
+  //     //     if (test != null && !test) await getAutoStartPermission();
+  //     //   } on PlatformException catch (e) {
+  //     //     log(e.toString());
+  //     //   }
+  //     //   if (!mounted) return;
+  //   //   }
+  //   // });
+  // }
 
   @override
   Widget build(BuildContext context) {
